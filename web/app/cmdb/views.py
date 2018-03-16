82d51523ffb274dde8264e17ea891bec2cc39da0
JerryBaby <dw.jerry.c@gmail.com>
2018-03-16 18:16:38 +0800

    切课增加多线程

--- web/app/cmdb/views.py
+++ web/app/cmdb/views.py
@@ -1,12 +1,66 @@
 #-*- coding: utf-8 -*-
+import threading
 from flask import request, jsonify
 import requests
 from . import cmdb
 
 
-API_VK1 = 'http://localhost/api/invoker/service/test/changeDBtoVK/class/{classId}?operatorId=666666'
-API_VK2 = 'http://localhost/api/invoker/service/class/{classId}/newLine/S9_V1_L2?operatorId=666666&operatorType=Staff'
-API_DB2 = 'http://localhost/api/invoker/service/class/{classId}/newLine/S4_V0_L2?operatorId=666666&operatorType=Staff'
+API_VK1 = 'http://localhost/api/invoker/service/test/changeDBtoVK/class/{classID}?operatorId=666666'
+API_VK2 = 'http://localhost/api/invoker/service/class/{classID}/newLine/S9_V1_L2?operatorId=666666&operatorType=Staff'
+API_DB2 = 'http://localhost/api/invoker/service/class/{classID}/newLine/S4_V0_L2?operatorId=666666&operatorType=Staff'
+
+
+class SwitchLineThread(threading.Thread):
+    def __init__(self, semaphore, apis, classid, succ, fail):
+        super(SwitchLineThread, self).__init__()
+        self.semaphore = semaphore
+        self.apis = apis
+        self.classid = classid
+        self.succ = succ
+        self.fail = fail
+
+    def run(self):
+        if len(self.apis) == 1:
+            self.semaphore.acquire()
+            if self.classid.isdigit():
+                url = self.apis[0].format(classID=self.classid)
+                try:
+                    r = requests.put(url, timeout=10)
+                    r.raise_for_status()
+                    if r.json().get('success'):
+                        self.succ.append(self.classid)
+                    else:
+                        fail.append(self.classid)
+                except Exception as e:
+                    print e
+                    fail.append(self.classid)
+            else:
+                self.fail.append(self.classid)
+            self.semaphore.release()
+
+        if len(self.apis) == 2:
+            self.semaphore.acquire()
+            if self.classid.isdigit():
+                url_1 = self.apis[0].format(classID=self.classid)
+                url_2 = self.apis[1].format(classID=self.classid)
+                try:
+                    r1 = requests.put(url_1, timeout=5)
+                    r1.raise_for_status()
+                    if r1.json().get('success'):
+                        r2 = requests.put(url_2, timeout=5)
+                        r2.raise_for_status()
+                        if r2.json().get('success'):
+                            self.succ.append(self.classid)
+                        else:
+                            fail.append(self.classid)
+                    else:
+                        fail.append(self.classid)
+                except Exception as e:
+                    print e
+                    fail.append(self.classid)
+            else:
+                self.fail.append(self.classid)
+            self.semaphore.release()
 
 
 @cmdb.route('/switch_vk1', methods=['POST'])
@@ -15,25 +69,11 @@ def switch_vk1():
     fail = []
     data = request.json
     if data is not None and data.get('classid'):
-        for i in data['classid'].split('\n'):
-            if i.isdigit():
-                url = API_VK1.format(classId=i)
-                try:
-                    r = requests.put(url, timeout=10)
-                    r.raise_for_status()
-                    if not r.json().get('success'):
-                        print 'failed: %s' % i
-                        fail.append(i)
-                    else:
-                        print 'success: %s' % i
-                        succ.append(i)
-                except Exception as e:
-                    print e
-                    print 'failed: %s' % i
-                    fail.append(i)
-            else:
-                print 'failed: %s' % i
-                fail.append(i)
+        semaphore = threading.Semaphore(10)
+        for id in data['classid'].split('\n'):
+            t = SwitchLineThread(semaphore, [API_VK1], id, succ, fail)
+            t.start()
+            t.join()
         return jsonify({'code': 0, 'result': {'success': succ, 'failed': fail}})
     else:
         return jsonify({'code': 1, 'result': 'invalid parameters.'})
@@ -45,27 +85,11 @@ def switch_vk2():
     fail = []
     data = request.json
     if data is not None and data.get('classid'):
-        for i in data['classid'].split('\n'):
-            if i.isdigit():
-                url_1 = API_VK1.format(classId=i)
-                url_2 = API_VK2.format(classId=i)
-                try:
-                    r1 = requests.put(url_1, timeout=5)
-                    r1.raise_for_status()
-                    if r1.json().get('success'):
-                        r2 = requests.put(url_2, timeout=5)
-                        r2.raise_for_status()
-                        if r2.json().get('success'):
-                            succ.append(i)
-                        else:
-                            fail.append(i)
-                    else:
-                        fail.append(i)
-                except Exception as e:
-                    print e
-                    fail.append(i)
-            else:
-                fail.append(i)
+        semaphore = threading.Semaphore(10)
+        for id in data['classid'].split('\n'):
+            t = SwitchLineThread(semaphore, [API_VK1, API_VK2], id, succ, fail)
+            t.start()
+            t.join()
         return jsonify({'code': 0, 'result': {'success': succ, 'failed': fail}})
     else:
         return jsonify({'code': 1, 'result': 'invalid parameters.'})
@@ -77,21 +101,11 @@ def switch_db2():
     fail = []
     data = request.json
     if data is not None and data.get('classid'):
-        for i in data['classid'].split('\n'):
-            if i.isdigit():
-                url = API_DB2.format(classId=i)
-                try:
-                    r = requests.put(url, timeout=10)
-                    r.raise_for_status()
-                    if not r.json().get('success'):
-                        fail.append(i)
-                    else:
-                        succ.append(i)
-                except Exception as e:
-                    print e
-                    fail.append(i)
-            else:
-                fail.append(i)
+        semaphore = threading.Semaphore(10)
+        for id in data['classid'].split('\n'):
+            t = SwitchLineThread(semaphore, [API_DB2], id, succ, fail)
+            t.start()
+            t.join()
         return jsonify({'code': 0, 'result': {'success': succ, 'failed': fail}})
     else:
         return jsonify({'code': 1, 'result': 'invalid parameters.'})
